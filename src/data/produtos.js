4aa4d21f30138568310a6d9be861e8dad61692f5
Layana Roberta <layrobertag4@gmail.com>
2021-11-25 11:43:36 -0300

    Incluindo imóveis próprios
    Adicionando imóveis próprios e seus valores avaliados

--- src/data/produtos.js
+++ src/data/produtos.js
@@ -1,5 +1,8 @@
 export default [
-    { id: 1, nome: 'Caneta', preco: 7.59 },
-    { id: 2, nome: 'Lapis', preco: 3.89 },
-    { id: 3, nome: 'Caderno', preco: 18.30 },
+    { id: 1, nome: 'Apartamendo em Maceió-Al', Valor: 100.000 },
+    { id: 2, nome: 'Fazenda Em Alagoas', preco: 500.000 },
+    { id: 3, nome: 'Pousada no Francês-AL', preco: 300.000 },
+    { id: 4, nome: 'Pousada em São Miguel dos Milagres-AL', preco: 350.000},
+    { id: 5, nome: 'Empresa em São Paulo', preco: 500.000},
+    { id: 6, nome: 'Filial em Recife', preco: 200.000},
 ]
